e77d2a1ed812ee849fcbdbf17aa558f160f080b6
Mike <qweqwenet@gmail.com>
2024-01-16 17:47:30 -0500

    added packages
    - injectable
    - get_it
    - build_runner
    
    for generate injectables use:
    dart run build_runner build --delete-conflicting-outputs

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:client_it/app/ui/main_app_builder.dart';
 import 'package:client_it/app/ui/main_app_runner.dart';
 
 void main() {
-  final appRunner = MainAppRunner();
+  const env = String.fromEnvironment("env", defaultValue: "dev");
+  const appRunner = MainAppRunner(env);
   final appBuilder = MainAppBuilder();
   appRunner.run(appBuilder);
 }
