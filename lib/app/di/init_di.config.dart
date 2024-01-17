2dee826e3aadb74f5792b542f554f834ee23cd23
Mike <qweqwenet@gmail.com>
2024-01-16 19:07:08 -0500

    injected AppConfig for dev, prod, test

--- lib/app/di/init_di.config.dart
+++ lib/app/di/init_di.config.dart
@@ -8,8 +8,14 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:client_it/app/data/main_app_config.dart' as _i4;
+import 'package:client_it/app/domain/app_config.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+
+const String _prod = 'prod';
+const String _dev = 'dev';
+const String _test = 'test';
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -17,10 +23,22 @@ extension GetItInjectableX on _i1.GetIt {
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
   }) {
-    _i2.GetItHelper(
+    final gh = _i2.GetItHelper(
       this,
       environment,
       environmentFilter,
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.ProdAppConfig(),
+      registerFor: {_prod},
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.DevAppConfig(),
+      registerFor: {_dev},
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.TestAppConfig(),
+      registerFor: {_test},
     );
     return this;
   }
