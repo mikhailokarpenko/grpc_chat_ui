59c2100a396b495293e34d1888ead1700eeb5ec0
Mike <qweqwenet@gmail.com>
2024-01-22 13:04:34 -0500

    login screen in process

--- lib/feature/auth/ui/login_screen.dart
+++ lib/feature/auth/ui/login_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  final controllerLogin = TextEditingController();
+  final controllerPassword = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +13,52 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("LoginScreen"),
       ),
+      body: Form(
+        key: formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  validator: emptyValidator,
+                  maxLines: 1,
+                  controller: controllerLogin,
+                  decoration: const InputDecoration(
+                      labelText: "Login", border: OutlineInputBorder()),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  validator: emptyValidator,
+                  maxLines: 1,
+                  controller: controllerPassword,
+                  decoration: const InputDecoration(
+                      labelText: "Password", border: OutlineInputBorder()),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState?.validate() == true) {
+                        print("OK");
+                      }
+                    },
+                    style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all<Size>(
+                            const Size(double.maxFinite, 40))),
+                    child: const Text("SignIn")),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  String? emptyValidator(String? text) {
+    if (text?.isEmpty == true) {
+      return "Field is empty";
+    }
+    return null;
   }
 }
